e320c5077e8b31114b1f139ebbeb0d75f030018b
bruno78 <devbruno78@gmail.com>
2017-12-12 21:27:51 -0500

    Add displaying card for subscribers

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,4 +1,4 @@
-$(document).on("turbolinks:load", function(){
+$(document).on("turbolinks:load", function() {
   stripe_public_key = $("meta[name='stripe-public-key']").attr("content");
   Stripe.setPublishableKey(stripe_public_key);
 
@@ -30,16 +30,12 @@ function stripeResponseHandler(status, response) {
     // Get the token ID:
     var token = response.id;
 
-    // Uncomment this line to see a CC card object on the console but encrypted. All you will be allowed to see
-    // is the brand, expiration date and the last four digits.
-    // console.log(response);
-
     // Insert the token ID into the form so it gets submitted to the server:
     $form.append($('<input type="hidden" name="stripeToken">').val(token));
     $form.append($('<input type="hidden" name="card_brand">').val(response.card.brand));
+    $form.append($('<input type="hidden" name="card_last4">').val(response.card.last4));
     $form.append($('<input type="hidden" name="card_exp_month">').val(response.card.exp_month));
     $form.append($('<input type="hidden" name="card_exp_year">').val(response.card.exp_year));
-    $form.append($('<input type="hidden" name="card_exp_last4">').val(response.card.last4));
 
     // Submit the form:
     $form.get(0).submit();
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,13 +6,21 @@ class SubscriptionsController < ApplicationController
 
   def create
     customer = current_user.stripe_customer
+
     begin
       subscription = customer.subscriptions.create(
         source: params[:stripeToken],
-        plan: params[:plan],
+        plan: params[:plan]
       )
 
       current_user.assign_attributes(stripe_subscription_id: subscription.id)
+      current_user.assign_attributes(
+        card_brand: params[:card_brand],
+        card_last4: params[:card_last4],
+        card_exp_month: params[:card_exp_month],
+        card_exp_year: params[:card_exp_year]
+      ) if params[:card_last4]
+
       current_user.save
 
       flash.notice = "Thanks for subscribing!"
