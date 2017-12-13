908e2a8fbab49e94e3dd8c1b697c6ac49e38a99d
bruno78 <devbruno78@gmail.com>
2017-12-12 21:51:50 -0500

    Add update and display cc functionality

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -30,4 +30,32 @@ class SubscriptionsController < ApplicationController
       render action: :new
     end
   end
+
+  def show
+  end
+
+  def update
+    customer = current_user.stripe_customer
+
+    begin
+      source = customer.sources.create(source: params[:stripeToken])
+      customer.default_source = source.id
+      customer.save
+
+      current_user.assign_attributes(
+        card_brand: params[:card_brand],
+        card_last4: params[:card_last4],
+        card_exp_month: params[:card_exp_month],
+        card_exp_year: params[:card_exp_year]
+      )
+
+      current_user.save
+
+      flash.notice = "Your card was updated successfully"
+      redirect_to root_path
+
+    rescue Stripe::CardError => e
+      flash.alert = e.message
+    end
+  end
 end
