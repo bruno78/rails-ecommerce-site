499ea32617a5d4a5ee4e2c416753f2392f057b83
bruno78 <devbruno78@gmail.com>
2017-12-12 20:00:34 -0500

    Add Stripe on the server side and additional handling errors

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,8 +1,25 @@
 class SubscriptionsController < ApplicationController
-  def new
+  before_action :authenticate_user!
 
+  def new
   end
 
   def create
+    customer = current_user.stripe_customer
+    begin
+      subscription = customer.subscriptions.create(
+        source: params[:stripeToken],
+        plan: params[:plan],
+      )
+
+      current_user.assign_attributes(stripe_subscription_id: subscription.id)
+      current_user.save
+
+      flash.notice = "Thanks for subscribing!"
+      redirect_to root_path
+    rescue Stripe::CardError => e
+      flash.alert = e.message
+      render action: :new
+    end
   end
 end
